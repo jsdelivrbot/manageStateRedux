1715f917aaefddfad5dcda507b14f29b2b06b2ef
Clint Ast <clintast@gmail.com>
2017-02-17 00:49:53 -0600

    added a book to the books reducer

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -3,6 +3,7 @@ export default function() {
     {title: 'Javascript: The Good Parts', pages: 972},
     {title: 'Harry Potter', pages: 1234},
     {title: 'The Dark Tower', pages: 2323},
-    {title: 'Eloquent Ruby', pages: 50}
+    {title: 'Eloquent Ruby', pages: 50},
+    {title: 'Clint the Great', pages: 598}
   ]
 }
